66dd1afd59a7a4fa5898f1cb29c3adf0fe0d37b7
Omulosi <mulongojohnpaul@gmail.com>
2020-02-14 18:52:05 +0300

    Update styles

--- assets/js/categories.js
+++ assets/js/categories.js
@@ -1,6 +1,8 @@
 const categories = document.querySelectorAll("[data-tag]");
 
-categories.forEach(category => {
-  category.addEventListener('click', () => {
-  })
-})
+const query = window.location.search.substring(1);
+const searchPrompt = document.querySelector('#search-prompt');
+
+let prompt = `Search for ${query} by typing it into the search bar`;
+
+searchPrompt.innerHtml = prompt;
